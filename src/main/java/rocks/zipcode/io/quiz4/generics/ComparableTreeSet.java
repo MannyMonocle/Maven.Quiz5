74104ecf5524cce688928256cea14c586b4f3b32
MannyMonocle <eetumadu@gmail.com>
2020-01-06 21:14:23 -0500

    I think I just missed my bus

--- src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/ComparableTreeSet.java
@@ -1,17 +1,21 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class    ComparableTreeSet<_> {
+public class ComparableTreeSet<_> extends TreeSet<_> implements Comparable<ComparableTreeSet<_>>{
+
+
     public ComparableTreeSet(_... arr) {
     }
-
-
     public ComparableTreeSet() {
     }
 
     public int compareTo(ComparableTreeSet<_> o) {
-        return Integer.valueOf(null);
+        return 1;
     }
+
 }
