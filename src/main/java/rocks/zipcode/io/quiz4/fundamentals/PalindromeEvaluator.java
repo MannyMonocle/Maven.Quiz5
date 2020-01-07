bbe2068ed763feacfb31998ff508e06743ccebd9
MannyMonocle <eetumadu@gmail.com>
2020-01-06 19:10:14 -0500

    get all palindromes

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,15 +1,23 @@
 package rocks.zipcode.io.quiz4.fundamentals;
+import rocks.zipcode.io.quiz4.fundamentals.StringEvaluator;
+
+import java.util.ArrayList;
 
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        ArrayList<String> list = new ArrayList<>();
+        String[] all = StringEvaluator.getAllSubstrings(string);
+        for(String sub : all){
+            if(isPalindrome(sub)){ list.add(sub);}
+        }
+        return list.toArray(new String[0]);
     }
 
     public static Boolean isPalindrome(String string) {
-        return string == reverseString(string);
+        return string.equals(reverseString(string));
     }
 
     public static String reverseString(String string) {
